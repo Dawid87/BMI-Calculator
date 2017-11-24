37e2d855a527e1fb7c01971fd58326f6cbb1a4b3
Dawid Delimata <delimata.dawid@gmail.com>
2017-11-24 23:32:17 +0000

    Action bar experiment

--- app/src/main/java/com/dawid/dawiddelimata/bmicalculator_ver14/MainActivity.java
+++ app/src/main/java/com/dawid/dawiddelimata/bmicalculator_ver14/MainActivity.java
@@ -1,31 +1,19 @@
 package com.dawid.dawiddelimata.bmicalculator_ver14;
 
-import android.app.Activity;
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.os.Handler;
-import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ProgressBar;
@@ -39,12 +27,8 @@ import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Date;
 
-import static android.os.Environment.getExternalStorageDirectory;
+import static com.dawid.dawiddelimata.bmicalculator_ver14.R.drawable.mainmenuback;
 
 public class MainActivity extends AppCompatActivity  implements BillingProcessor.IBillingHandler  {
 
@@ -275,6 +259,7 @@ public class MainActivity extends AppCompatActivity  implements BillingProcessor
 
         getSupportActionBar().setTitle("    BMI Calculator");                                           // Set name of new toolbar
         getSupportActionBar().setLogo(R.drawable.scale);                                                // Set toolbar icon
+
     }
 
     // Create three dot toolbar menu
